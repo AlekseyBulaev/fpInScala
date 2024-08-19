e76f9e3457a109e34a09d3caeee822d7c83e7fb6
AlekseyBulaev <alexeybulaev@gmail.com>
2024-08-18 23:20:08 -0400

    000 - add foldRight listing

--- src/main/scala/fp/in/scala/part1/Chapter3.scala
+++ src/main/scala/fp/in/scala/part1/Chapter3.scala
@@ -2,4 +2,8 @@ package fp.in.scala.part1
 
 object Chapter3 {
 
+  def foldRight[A,B](as: List[A], z: B)(f: (A,B) => B): B = as match {
+    case ::(head, next) => f(head, foldRight(next, z)(f))
+    case Nil => z
+  }
 }
