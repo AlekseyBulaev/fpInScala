a3e63490f4857e81ebbe2d95575fa9fad65c23f2
AlekseyBulaev <alexeybulaev@gmail.com>
2024-09-04 22:18:20 -0400

    sol004 - solution for 4.6 exercise

--- src/main/scala/fp/in/scala/part1/Chapter4.scala
+++ src/main/scala/fp/in/scala/part1/Chapter4.scala
@@ -57,14 +57,18 @@ object Chapter4 {
     }
 
     def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
-      case Left(value) => b
+      case Left(_) => b
       case _ => this
     }
 
     def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = this match {
-      case Left(value) => ???
-      case Right(value) => ???
+      case Left(e) => Left(e)
+      case Right(v) => b map(f(v,_))
     }
+    def map2ViaForComprehension[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = for {
+      a <- this
+      b1 <- b
+    } yield f(a, b1)
   }
 
   case class Left[+E](value: E) extends Either[E, Nothing]
