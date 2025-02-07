f1d2d2860c9a6191d18c17f817503044e4a9b2c0
AlekseyBulaev <alexeybulaev@gmail.com>
2025-02-07 00:06:53 -0500

    ch002 - add tests for ExercisesChapter2

--- src/test/scala/fp/in/scala/part1/ExercisesChapter2Test.scala
+++ src/test/scala/fp/in/scala/part1/ExercisesChapter2Test.scala
@@ -9,6 +9,24 @@ class ExercisesChapter2Test extends AnyFlatSpec {
   }
 
   "sorted array" should "return true" in {
-    assert(isSorted(Array(1, 2, 3, 4, 5), (a: Int, b: Int) => a > b))
+    assert(isSorted(Array(5, 4, 3, 2, 1), (a: Int, b: Int) => a > b))
+  }
+
+  "curried function" should "return from function" in {
+    def f(a: Int, b: Int) = a + b
+    assert(curry(f)(10)(5) == 15)
+  }
+
+  "uncurried function" should "return from function" in {
+    def f(a: Int)(b: Int) =  a + b
+    def uncurryFun = uncurry(f)
+    assert(uncurryFun(10,5) == 15)
+  }
+
+  "compose function" should "return from function" in {
+    def f: Int => Int = a => a * 2
+    def g: Int => Int = b => b + 2
+
+    assert(compose(f, g)(5) == 14)
   }
 }
